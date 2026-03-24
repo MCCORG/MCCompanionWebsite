39f3ae49bce7dcea2d68165d88532e66251f4076
Hasan <52300558+hasankayra04@users.noreply.github.com>
2026-03-24 05:57:45 +0300

    Revert "Nrouter (#4)"
    This reverts commit fc469b43fd081ed7d1b05e10df288347ad28fa04.

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './App.css'
 
-// Handle GitHub Pages redirect
-const redirect = sessionStorage.redirect;
-if (redirect) {
-  delete sessionStorage.redirect;
-  window.history.replaceState(null, null, redirect);
-}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 )
